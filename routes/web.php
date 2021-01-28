63ee1ace61fb286406a0b9c09f385ac0eb5e8025
danasugu <dana.sugu@gmail.com>
2021-01-28 19:55:24 +0200

    adding pages

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('info');
 });
+
+// Route::get('/info', function () {
+//     return view('info');
+// });
 
 Auth::routes();
 
